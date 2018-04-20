d3e6280b185597af8cc8e81689ca880e2d99b4ab
Ador <adroit.ador@live.com>
2018-04-20 23:51:48 +0600

    ALPHA

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -339,7 +339,7 @@ $(function () {
 */
 
 function initMap() {
-	var myLatlng = new google.maps.LatLng(40.773328,-73.960088); // <- Your latitude and longitude
+	var myLatlng = new google.maps.LatLng(23.1759421,90.20549099999994); // <- Your latitude and longitude
 	var styles = [
 	{
 		"featureType": "water",
